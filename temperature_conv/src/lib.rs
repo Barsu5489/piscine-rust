2c9b05c824cd2727f20aed9b8d0d069eba2fd402
ebarsula <ebarsulai@gmail.com>
2025-04-02 00:57:17 +0300

    fix: convertor

--- temperature_conv/src/lib.rs
+++ temperature_conv/src/lib.rs
@@ -1,11 +1,11 @@
 
-fn main() {
-    println!("{} F = {} C", -459.67, fahrenheit_to_celsius(-459.67));
-    println!("{} C = {} F", 0.0, celsius_to_fahrenheit(0.0));
-}
+// fn main() {
+//     println!("{} F = {} C", -459.67, fahrenheit_to_celsius(-459.67));
+//     println!("{} C = {} F", 0.0, celsius_to_fahrenheit(0.0));
+// }
 
 pub fn fahrenheit_to_celsius(f: f64) -> f64 {
-    (f - 32.0) * (9.0/5.0 - 56.0/45.0 )
+   round( 2, ((f - 32.0) * (9.0/5.0 - 56.0/45.0 )))
 }
 
 pub fn celsius_to_fahrenheit(c: f64) -> f64 {
