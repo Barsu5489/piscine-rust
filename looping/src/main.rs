f1c75d9029ae84ecd622b2dd63718a6fc0291b07
ebarsula <ebarsulai@gmail.com>
2025-04-02 02:59:00 +0300

    fix: trim

--- looping/src/main.rs
+++ looping/src/main.rs
@@ -2,8 +2,9 @@ use std::io;
 
 fn main() -> io::Result<()> {
     // The riddle and the correct answer
-    let riddle = "I am the beginning of the end, and the end of time and space. \
+    let riddl = "I am the beginning of the end, and the end of time and space. \
                   I am essential to creation, and I surround every place. What am I?";
+                  let riddle = riddl.trim()
     let correct_answer = "The letter e";
 
     let mut attempts = 0;
@@ -22,8 +23,8 @@ fn main() -> io::Result<()> {
         attempts += 1;
 
         // Check if the input is correct
-        if input.trim() == correct_answer {
-            println!("Number of trials:{} ", attempts);
+        if input.trim() == correct_answer.trim() {
+            println!("Number of trials: {} ", attempts);
             break;
         } 
     }
