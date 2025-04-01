c50944e803f9fd214693d2be4f40afba593f4f0c
ebarsula <ebarsulai@gmail.com>
2025-04-02 01:03:07 +0300

    fix: temps

--- temperature_conv/src/lib.rs
+++ temperature_conv/src/lib.rs
@@ -1,11 +1,7 @@
 
-// fn main() {
-//     println!("{} F = {} C", -459.67, fahrenheit_to_celsius(-459.67));
-//     println!("{} C = {} F", 0.0, celsius_to_fahrenheit(0.0));
-// }
-
+// cd ..
 pub fn fahrenheit_to_celsius(f: f64) -> f64 {
-   round( 2, ((f - 32.0) * (9.0/5.0 - 56.0/45.0 )))
+    (f - 32.0) * (9.0/5.0 - 56.0/45.0 )
 }
 
 pub fn celsius_to_fahrenheit(c: f64) -> f64 {
