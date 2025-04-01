22494ff4ae3b689a2fb3ea719a38540ab2114f33
ebarsula <ebarsulai@gmail.com>
2025-04-02 00:39:51 +0300

    temperature convertor

---
+++ temperature_conv/src/lib.rs
@@ -0,0 +1,26 @@
+
+fn main() {
+    println!("{} F = {} C", -459.67, fahrenheit_to_celsius(-459.67));
+    println!("{} C = {} F", 0.0, celsius_to_fahrenheit(0.0));
+}
+
+pub fn fahrenheit_to_celsius(f: f64) -> f64 {
+    (f - 32 * 9/5 )- (f - 32 * 56/45) 
+}
+
+pub fn celsius_to_fahrenheit(c: f64) -> f64 {
+    c * 9/5 + 32
+
+}  
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
