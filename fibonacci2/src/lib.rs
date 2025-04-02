cdba01d2b97e574f1e538152f904f9f56e349667
ebarsula <ebarsulai@gmail.com>
2025-04-02 05:23:24 +0300

    fibo

---
+++ fibonacci2/src/lib.rs
@@ -0,0 +1,20 @@
+pub fn fibonacci(n: u32) -> u32 {
+    if n == 0 {
+        return 0;
+    } else if n == 1 {
+        return 1;
+    }
+    
+    fibonacci(n - 1) + fibonacci(n - 2)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
