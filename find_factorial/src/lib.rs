4e41a3ad4752aaa628c20b1eb9f691fe68ac6221
ebarsula <ebarsulai@gmail.com>
2025-04-02 04:36:22 +0300

    factorial

---
+++ find_factorial/src/lib.rs
@@ -0,0 +1,24 @@
+pub fn factorial(num: u64) -> u64 {
+    if num ==0 || num ==1{
+        return 1
+    }
+    return num * factorial(num-1)
+}
+fn main() {
+    println!("The factorial of 0 = {}", factorial(0));
+    println!("The factorial of 1 = {}", factorial(1));
+    println!("The factorial of 5 = {}", factorial(5));
+    println!("The factorial of 10 = {}", factorial(10));
+    println!("The factorial of 19 = {}", factorial(19));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
