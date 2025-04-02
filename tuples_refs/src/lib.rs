eff808e9d3eea124d293377bd69064d70919a886
ebarsula <ebarsulai@gmail.com>
2025-04-02 20:34:55 +0300

    tuple strusct

---
+++ tuples_refs/src/lib.rs
@@ -0,0 +1,18 @@
+pub struct Student(u32, String, String);
+
+// Function to return the id of the student
+pub fn id(student: &Student) -> u32 {
+    student.0
+}
+
+// Function to return the first name of the student
+pub fn first_name(student: &Student) -> &str {
+    &student.1
+}
+
+// Function to return the last name of the student
+pub fn last_name(student: &Student) -> &str {
+    &student.2
+}
+
+
