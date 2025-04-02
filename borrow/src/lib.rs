2a2ae1e1c4f852c418facfdc942fa2e08a28a6b0
ebarsula <ebarsulai@gmail.com>
2025-04-02 22:53:27 +0300

    slice fix

--- borrow/src/lib.rs
+++ borrow/src/lib.rs
@@ -4,18 +4,3 @@ return s.len()
 
 
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_str_len_single_char() {
-        let result = str_len("a"); // Test with a string of one character
-        assert_eq!(result, 1); // The length should be 1
-    }
-    #[test]
-    fn test_str_lenmult_char(){
-        let result = str_len("hello");
-        assert_eq!(result, 5)
-    }
-}
