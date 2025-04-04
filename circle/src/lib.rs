be6d77d023482858e9b6cbaababddb5faa50a87e
ebarsula <ebarsulai@gmail.com>
2025-04-05 00:28:29 +0300

    circle

---
+++ circle/src/lib.rs
@@ -0,0 +1,40 @@
+#[derive(Debug, Clone, Copy)]
+pub struct Point(f64, f64);
+
+impl Point {
+    pub fn distance(&self, other: Point) -> f64 {
+        let dx = self.0 - other.0;
+        let dy = self.1 - other.1;
+        (dx.powi(2) + dy.powi(2)).sqrt()
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct Circle {
+    pub center: Point,
+    pub radius: f64,
+}
+
+impl Circle {
+    pub fn new(x: f64, y: f64, radius: f64) -> Self {
+        Self {
+            center: Point(x, y),
+            radius,
+        }
+    }
+
+    pub fn diameter(&self) -> f64 {
+        self.radius * 2.0
+    }
+
+    pub fn area(&self) -> f64 {
+        std::f64::consts::PI * self.radius * self.radius
+    }
+
+    pub fn intersect(&self, other: Circle) -> bool {
+        let distance = self.center.distance(other.center);
+        distance < (self.radius + other.radius)
+    }
+}
+
+
