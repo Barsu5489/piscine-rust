5c007259a59026f665aad2a1a3c95b18a4c05ff9
ebarsula <ebarsulai@gmail.com>
2025-04-05 22:37:12 +0300

    bigger

---
+++ bigger/src/lib.rs
@@ -0,0 +1,13 @@
+use std::collections::HashMap;
+
+pub fn bigger(h: HashMap<&str, i32>) -> i32 {
+    let mut max = 0;
+
+    for &value in h.values() {
+        if value > max {
+            max = value;
+        }
+    }
+
+    max
+}
