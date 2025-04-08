d54e47b60dbbbc8e2144787afa66a7a4075adcb8
ebarsula <ebarsulai@gmail.com>
2025-04-08 22:02:01 +0300

    unwrap

--- unwrap_or_expect/src/lib.rs
+++ unwrap_or_expect/src/lib.rs
@@ -26,17 +26,3 @@ pub fn fetch_data(server: Result<&str, &str>, security_level: Security) -> Strin
     }
 }
 
-fn main() {
-    println!("{}", fetch_data(Ok("server1.com"), Security::Warning));
-    println!("{}", fetch_data(Err("server.com"), Security::Warning));
-    println!("{}", fetch_data(Err("server2.com"), Security::NotFound));
-
-    // Panics with no custom message
-    // fetch_data(Err("ERROR CRITICAL"), Security::Unknown);
-
-    // Panics with the message "ERROR: program stops"
-    // fetch_data(Err("server.com"), Security::Message);
-
-    // Panics with the message "malicious_server.com"
-    // fetch_data(Ok("malicious_server.com"), Security::UnexpectedUrl);
-}
