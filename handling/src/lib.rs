6b56a3e438122556c3574ad641f8881bd624d836
ebarsula <ebarsulai@gmail.com>
2025-04-09 01:35:30 +0300

    fix

--- handling/src/lib.rs
+++ handling/src/lib.rs
@@ -1,11 +1,14 @@
-use std::path::Path;
 use std::fs::OpenOptions;
+use std::path::Path;
+use std::{write, fs};
 
 pub fn open_or_create<P: AsRef<Path>>(path: &P, content: &str) {
-   OpenOptions::new()
+   let mut file = fs::OpenOptions::new()
    .create(true) 
    .append(true)
    .open(&path)
    .unwrap();
+
+   file.write_all(content.as_bytes()).unwrap()
 
 }
