a13c9b8fe2967d87e0c98d12ad0b574d58d8f6d0
ebarsula <ebarsulai@gmail.com>
2025-04-08 23:00:25 +0300

    fix

--- unwrap_or_expect/src/lib.rs
+++ unwrap_or_expect/src/lib.rs
@@ -6,24 +6,23 @@ pub enum Security {
     UnexpectedUrl,
 }
 
-
-// Implement fetch_data with Result<&str, &str>
-
 pub fn fetch_data(server: Result<&str, &str>, security_level: Security) -> String {
-    match (server, security_level) {
-        (Ok(url), Security::Unknown) => url.to_string(),
-        (Err(_), Security::Unknown) => panic!(), // Panics with no message
-
-        (Ok(url), Security::Message) => url.to_string(),
-        (Err(_), Security::Message) => panic!("ERROR: program stops"), // Panics with message
-
-        (Ok(url), Security::Warning) => url.to_string(),
-        (Err(_), Security::Warning) => "WARNING: check the server".to_string(),
-
-        (Ok(url), Security::NotFound) => url.to_string(),
-        (Err(msg), Security::NotFound) => format!("Not found: {}", msg),
-
-        (Err(msg), Security::UnexpectedUrl) => msg.to_string(),
-        (Ok(url), Security::UnexpectedUrl) => panic!("{}", url), // Panics with server URL
+    match  server {
+        Ok(url) =>  {
+            match security_level {
+                Security::UnexpectedUrl => panic!("{url}"),
+                _ => format!("{url}"),
+            }
+    },
+        Err(e) => {
+            match security_level {
+                Security::Unknown => panic!("{e}"),
+                Security::Message => panic!("ERROR: program stops"),
+                Security::Warning => format!("WARNING: check the server"),
+                Security::NotFound => format!("Not found: {e}"),
+                Security::UnexpectedUrl => format!("{e}"),
+            }
+        }
     }
 }
+
