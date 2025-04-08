2510ff9e533f4f1c4a222e04c8e29cda41f383a1
ebarsula <ebarsulai@gmail.com>
2025-04-08 23:51:59 +0300

    fix: open fiile

--- panic/src/lib.rs
+++ panic/src/lib.rs
@@ -1,5 +1,17 @@
  use std::fs::{self, File};
 
 pub fn open_file(s: &str) -> File {
-    File::open(s).unwrap_or_else(|_| panic!("File '{}' not found!", s))
+    File::open(s).unwrap()
+}
+
+fn main() {
+    let filename = "created.txt";
+    File::create(filename).unwrap();
+
+    println!("{:?}", open_file(filename));
+
+    fs::remove_file(filename).unwrap();
+
+    // this should panic!
+    open_file(filename);
 }
